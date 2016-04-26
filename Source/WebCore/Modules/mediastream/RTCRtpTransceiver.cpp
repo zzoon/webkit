66fcd05c0ef29688c89efe8e89d3ce02f8a64473
Adam Bergkvist <adam.bergkvist@ericsson.com>
2016-04-26 12:13:34 +0200

    Update year in some copyright headers

--- Source/WebCore/Modules/mediastream/RTCRtpTransceiver.cpp
+++ Source/WebCore/Modules/mediastream/RTCRtpTransceiver.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Ericsson AB. All rights reserved.
+ * Copyright (C) 2016 Ericsson AB. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
